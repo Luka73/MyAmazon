774b2d359f2a4b95d2e687036b82aecb906b5c85
Luka73 <luanafernandes73@gmail.com>
2020-03-18 19:30:25 -0400

    initial commit

---
+++ src/main/java/com/example/myamazon/MyamazonApplication.java
@@ -0,0 +1,13 @@
+package com.example.myamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyamazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyamazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myamazon/MyamazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myamazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyamazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
